4b30c9d5ea600396ae0dd3239fab123893270e8a
CroiXan <xan.mark.vi@gmail.com>
2025-06-19 21:24:29 -0400

    openfeign gateway

--- src/main/java/com/certichain/document/DocumentApplication.java
+++ src/main/java/com/certichain/document/DocumentApplication.java
@@ -2,8 +2,10 @@ package com.certichain.document;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class DocumentApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/certichain/document/client/BlockChainGatewayClient.java
@@ -0,0 +1,64 @@
+package com.certichain.document.client;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.certichain.document.model.PrivateDocument;
+import com.certichain.document.model.PrivateDocumentAuditLog;
+import com.certichain.document.model.PublicDocument;
+import com.certichain.document.model.PublicDocumentAuditLog;
+
+@FeignClient(name = "gateway-service", url = "http://grupo8jwt.ddns.net:8080/")
+public interface BlockChainGatewayClient {
+
+    @PostMapping("/privatedocuments")
+    public void savePrivateDocument(@RequestBody PrivateDocument doc);
+
+    @PutMapping("/privatedocuments/{id}/state")
+    public void updateState(@PathVariable String id, @RequestParam String newState);
+
+    @GetMapping("/privatedocuments/{id}")
+    public PrivateDocument getPrivateDocument(@PathVariable String id);
+
+    @GetMapping("/privatedocuments/institution/{institution}")
+    public List<PrivateDocument> privateQueryByInstitution(@PathVariable String institution);
+
+    @GetMapping("/privatedocuments/user/{userId}")
+    public List<PrivateDocument> privateQueryByUser(@PathVariable String userId);
+
+    @GetMapping("/privatedocuments/audit")
+    public List<PrivateDocumentAuditLog> queryPrivateAuditLogs(
+            @RequestParam String filterType,
+            @RequestParam String filterValue,
+            @RequestParam String startDate,
+            @RequestParam String endDate
+    );
+
+    @PostMapping("/publicdocuments")
+    public void registerDocument(@RequestBody PublicDocument document);
+
+    @GetMapping("/publicdocuments/{id}")
+    public PublicDocument getDocumentById(@PathVariable String id);
+
+    @GetMapping("/publicdocuments/institution/{institution}")
+    public List<PublicDocument> publicQueryByInstitution(@PathVariable String institution);
+
+    @GetMapping("/publicdocuments/user/{userId}")
+    public List<PublicDocument> publicQueryByUser(@PathVariable String userId);
+
+    @GetMapping("/publicdocuments/audit")
+    public List<PublicDocumentAuditLog> queryAuditLogs(
+        @RequestParam String filterType,
+        @RequestParam String filterValue,
+        @RequestParam String startDate,
+        @RequestParam String endDate
+    );
+
+}
